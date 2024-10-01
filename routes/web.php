b68dd86468201d2c50bfc2eee04daaa1a87e5de3
mostafa sayed <83194711+Mostafa12-sayed@users.noreply.github.com>
2024-10-02 02:13:54 +0300

    first_commit

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('customer.product');
 });
 
 Route::get('/dashboard', function () {
